ef0cc9e39faaee8a7b97b3df00d0f6070ce6e7dc
Kevin Zagalo <kevin.zagalo@inria.fr>
2020-04-25 20:26:03 +0200

    example

--- predict_example.py
+++ predict_example.py
@@ -3,15 +3,19 @@ from pokedex import *
 
 if __name__ == '__main__':
     svm = import_model('svm.pkl')
-    error = 0
-    for f in os.listdir(PATH):
-        img = load_img(f)
-        pkmn_img = img.ravel().reshape(1, -1)
-        pkmn_id = svm.predict(pkmn_img)
-        if pkmn_id[0] == int(f[:-4]):
-            pass
-        else:
-            print(POKEDEX[int(f[:-4])])
-            error += 1
-    print(error)
+    #error = 0
+    #for f in os.listdir(PATH):
+    f = 'PokecordSpawn.jpg'
+    img = load_img(f).reshape(SIZE, SIZE)
+    plt.imshow(img)
+    plt.show()
+    pkmn_img = img.ravel().reshape(1, -1)
+    pkmn_id = svm.predict(pkmn_img)
+
+    # if pkmn_id[0] == int(f[:-4]):
+    #     pass
+    # else:
+    print(POKEDEX[pkmn_id[0]])
+    #     error += 1
+    # print(error)
 
